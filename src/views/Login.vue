629e0ceaba9c1a32f0606413a82bf2a32e8230aa
andarasophan <andarasophan@gmail.com>
2021-08-19 07:30:01 +0700

    style: update login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,61 @@
 <template>
-  <div class="container">
-    <h1>Login</h1>
-    <b-form @submit="onSubmit">
-      <b-form-group
-        label="Username"
-        label-for="username"
-        :invalid-feedback="usernameError"
-        :state="usernameState"
-      >
-        <b-form-input
-          id="username"
-          v-model="username.value"
-          :state="usernameState"
-          @blur="setDirty('username')"
-        />
-      </b-form-group>
+  <div class="container-fluid">
+    <div :class="$style.loginContainer" class="row">
+      <div :class="$style.background" class="col-md-7"/>
+      <div class="col-md-5">
+        <b-form :class="$style.formWrapper" @submit="onSubmit">
 
-      <b-form-group
-        label="Password"
-        label-for="password"
-        :invalid-feedback="passwordError"
-        :state="passwordState"
-      >
-        <b-form-input
-          id="password"
-          v-model="password.value"
-          :state="passwordState"
-          @blur="setDirty('password')"
-          type="password"
-        />
-      </b-form-group>
+          <div class="d-flex align-items-center mb-3 justify-content-between">
+            <img class="mr-3" height="50px" alt="" src="../assets/pokemon-logo.png">
+            <h4 class="font-weight-bold text-black-50 mb-0">Login</h4>
+          </div>
 
-      <b-button
-        class="d-flex align-items-center justify-content-center w-100"
-        type="submit"
-        variant="primary"
-      >
-        <b-spinner v-if="isLoading" class="mr-1" small/>
-        Submit
-      </b-button>
-    </b-form>
+          <b-form-group
+            label="Username"
+            label-for="username"
+            :invalid-feedback="usernameError"
+            :state="usernameState"
+            label-size="sm"
+            label-class="mb-0 font-weight-bold"
+          >
+            <b-form-input
+              id="username"
+              v-model="username.value"
+              :state="usernameState"
+              @blur="setDirty('username')"
+            />
+          </b-form-group>
+
+          <b-form-group
+            label="Password"
+            label-for="password"
+            :invalid-feedback="passwordError"
+            :state="passwordState"
+            label-size="sm"
+            label-class="mb-0 font-weight-bold"
+          >
+            <b-form-input
+              id="password"
+              v-model="password.value"
+              :state="passwordState"
+              @blur="setDirty('password')"
+              type="password"
+            />
+          </b-form-group>
+
+          <b-button
+            class="d-flex align-items-center justify-content-center w-100 mt-4"
+            type="submit"
+            variant="outline-primary"
+            pill
+          >
+            <b-spinner v-if="isLoading" class="mr-1" small/>
+            Submit
+          </b-button>
+
+        </b-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -108,3 +124,29 @@ export default {
   }
 }
 </script>
+
+<style module lang="scss">
+.loginContainer {
+  min-height: 100vh;
+  .background {
+    background: url('../assets/pokemon-bg.jpg');
+    background-size: cover;
+    background-position: right;
+    clip-path: polygon(0 0,90% 0,100% 100%,0 100%);
+  }
+
+  .formWrapper {
+    padding: 3rem;
+  }
+
+  @include media-breakpoint-down(sm) {
+    .background {
+      display: none;
+    }
+
+    .formWrapper {
+      padding: 4rem 2rem;
+    }
+  }
+}
+</style>
